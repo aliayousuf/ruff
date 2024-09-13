10ef40db6730c81423a4d46bf785cdc45c952d47
aliayousuf <160315316+aliayousuf@users.noreply.github.com>
2024-09-13 12:45:30 +0500

    Add files via upload

---
+++ html/script.js
@@ -0,0 +1,115 @@
+
+
+
+var element;
+
+// element = document.getElementById("header").getAttribute("class");
+
+// element = document.getElementById("header").getAttribute("style");
+
+// element = document.getElementById("header").getAttributeNode("onclick");
+
+// element = document.getElementById("header").getAttributeNode("style").value;
+
+// element = document.getElementById("header").attributes[1];
+
+//  element = document.getElementById("header").attributes[2].name;
+
+// element = document.getElementById("header").innerHTML;
+// console.log(element);
+
+
+// document.getElementById("header").innerHTML = "Wow"
+// element = document.getElementById("header").innerHTML;
+
+// console.log(element);
+
+
+// document.getElementById("header").setAttribute("style","border: 10px dotted orange;");
+
+// document.getElementById("header").getAttribute("class");
+
+// console.log(element);
+
+
+// document.getElementById("header").attributes[1].value = "xyz";
+
+// element = document.getElementById("header").getAttribute("class");
+// console.log(element);
+
+
+// element = document.getElementById("header").removeAttribute("style")
+// document.getElementById("header").removeAttribute("class")
+// console.log(element);
+
+
+// element = document.querySelector("#header").innerHTML = "<h1>Wow</h1>";
+
+// element= document.querySelector(".list");
+
+// element = document.getElementsByClassName("list")
+
+// element= document.querySelectorAll(".list");
+
+// element= document.querySelectorAll(".list")[1];
+
+// element= document.querySelectorAll(".list")[1].innerHTML;
+
+
+// element = document.querySelectorAll("ul");
+// element = document.querySelectorAll("ul");
+
+// element = document.querySelectorAll("ul")[1];
+// console.log(element);
+
+
+// element = document.querySelectorAll("#header h1");
+
+// element = document.querySelectorAll("#header h1")[1].innerHTML;
+
+// console.log(element);
+
+
+// document.querySelector("#header").style.backgroundColor = "cyan";
+// document.querySelector("#header").style.color= "pink";
+
+// element = document.querySelector("#header").style.color;
+// console.log(element);
+
+
+// document.querySelector("#header").className = "abc xyz";
+// element = document.querySelector("#header").className;
+// console.log(element);
+
+
+// document.querySelector("#header").classList.add =("efg","xyz");
+// element = document.querySelector("#header").className;
+// console.log(element);
+
+
+
+document.querySelector("#header").classList.remove =("efg");
+element = document.querySelector("#header").className;
+ console.log(element);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
